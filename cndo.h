e495975bdb584ceb743bcf04b9cc5bf0157a6b1a
Jaycee Pang <jayceepang@berkeley.edu>
2024-03-15 06:08:12 -0700

    setting up to build fock matrix, and other needed for SCF claculation

--- cndo.h
+++ cndo.h
@@ -1,4 +1,8 @@
-
+#pragma #once 
+#include <iostream>
+#include <armadillo> 
+#include <map> 
+#include <vector> 
 /*
 General algorithm
 1. Guess initial P 
@@ -14,3 +18,30 @@ General algorithm
     arma::aprox_equal(Pa, Pa_old, "absdiff", tol)
 6. If converged, calculate total energy. Else, return to step 2 
 */
+
+#include "molecule.h"
+#include 
+class CNDO {
+    private:
+        arma::mat Fa_; // fock matrix alpha
+
+        Molecule molecule; 
+        arma::mat Pa_; // alpha density matrix 
+        arma::mat Pb_; // beta density matrix 
+        arma::mat P_; // density matrix --> alpha or beta should this class be interactive or for both 
+        arma::mat Gamma_; // gamma ab matrix
+        arma::mat Ca_, Cb_; // C beta matrix  // C alpha matrix
+ 
+        arma::mat Fb_; // beta Fock matrix 
+        int q, p; // num alpha and beta electrons
+
+    public: 
+        CNDO()
+        void build_F(); 
+        void build_G() {
+
+        }
+        
+
+};
+void SCF(); // driver code 
